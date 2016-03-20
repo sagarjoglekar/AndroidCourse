059e64a422c28f84046251934aceeaa2ad7c0862
Sagar Joglekar <sagarjoglekar@gmail.com>
2016-03-20 15:05:49 +0000

    Simple List project

---
+++ UdacityLauncher/app/src/main/java/launcher/course/udacity/udacitylauncher/MainActivity.java
@@ -0,0 +1,34 @@
+package launcher.course.udacity.udacitylauncher;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onCapstoneClick(View v){
+        Context context = getApplicationContext();
+        CharSequence text = "Launching capstone app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void onSpotifyStreamerClick(View v){
+        Context context = getApplicationContext();
+        CharSequence text = "Launching Spotify Streamer app!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
